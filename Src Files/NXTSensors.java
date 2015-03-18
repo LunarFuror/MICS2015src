f9b79a77f049a2a8a99b6e01d859fc0b61819714
Grayson Lorenz <lunar_furor@hotmail.com>
2015-03-18 12:41:41 -0500

    Update send/recieve, added decode()
    The decode will probably never be used in the nxt but this is the place it can be grabbed from and altered for later.
    Once connected a program can send a request of int 1 to recieve data back from the nxt.
    NXT will generateOutput() then send a sensor code which is basically an 8 bit sent as a boring old int.

--- Src Files/NXTSensors.java
+++ Src Files/NXTSensors.java
@@ -7,13 +7,14 @@ public class CommTest
 	public static DataOutputStream dataOut; 
 	public static DataInputStream dataIn;
 	public static USBConnection USBLink;
-	public static TouchSensor touchRight = new TouchSensor(SensorPort.S1);
-	public static TouchSensor touchLeft = new TouchSensor(SensorPort.S2);
-	public static ColorSensor colorFront = new ColorSensor(SensorPort.S3);
-	public static ColorSensor colorBack = new ColorSensor(SensorPort.S4);
-	public boolean request = false;
-	public boolean dataRecieved = false;
-	public boolean[] sensors = {false, false, false, false, false, false};
+	public static TouchSensor touchRightFront = new TouchSensor(SensorPort.S1);
+	public static TouchSensor touchLeftFront = new TouchSensor(SensorPort.S2);
+	public static TouchSensor touchRightBack = new TouchSensor(SensorPort.S3);
+	public static TouchSensor touchLeftBack = new TouchSensor(SensorPort.S4);//multiplexer 1
+	public static ColorSensor colorFront = new ColorSensor(SensorPort.S5);//multiplexer 2
+	public static ColorSensor colorBack = new ColorSensor(SensorPort.S6);//multiplexer 3
+	public int request = 0;
+	public int sensors = 0;
 	
 	public static void main(String[] args) { 
 		NXTConnection connection = null;
@@ -21,14 +22,42 @@ public class CommTest
 		while(!Button.ENTER.isDown()){}
 		connect();
 		
-		try {
-			dataOut.writeInt(1234);
-			System.out.println("NAILED IT!");
-		} catch (IOException e ) {
-			System.out.println(" write error "+e); 
+		//Loops while running and connected
+		while(true){
+			//Waits for something to read
+			while(dataIn.Available())
+			{
+				//tries to read it
+				try{
+					//if read, sets the request to it.
+					request = dataIn.ReadInt();
+				} catch (IOException e ) {
+					//IF READING FAILS
+					System.out.println("I CAN'T NOT READ NONE TOO GOOD!!!   ");
+				}
+				//if that request is 1, then NXT sends sensor data
+				//though I doubt we'll ever request anything else if we wanted to break this up
+				//change this if to a switch.
+				if(request == 1){
+					try {
+						//generate, write, print.
+						generateOutput();
+						dataOut.writeInt(sensors);
+						System.out.print(" " + sensors);
+					} catch (IOException e ) {
+						//IF WRITING FAILS
+						System.out.print("ALL WORK AND NO PLAY MAKES JACK A DULL BOY!!!   "); 
+					}
+					//clear request for data
+					request = 0;
+				}
+			}			
 		}
 	}   
 	
+	/**
+	*	Waits for a connection to the pc/pi. START NXT BEFORE TRYING TO CONNECT TO IT.
+	*/
 	public static void connect()
 	{  
 		System.out.println("Listening");
@@ -37,45 +66,102 @@ public class CommTest
 		dataIn = USBLink.openDataInputStream();
 	}
 	
+	/**
+	*	Resets the sensor data, then generates it by simply adding to the sensor integer.
+	*	The sensor integer basically is an 8bit number but ... you read it normally.
+	*	Math for decoding is below. You shouldn't be reading this anyway. Why are you reading this?
+	* 	Don't you trust my work?
+	*/
 	public static void generateOutput(){
-		//Right Touch
-		if(touchRight.isPressed())
-			sensors[0] = true;
-		else
-			sensors[0] = false;
+		sensors = 0;
 		
-		//Left Touch
-		if(touchLeft.isPressed())
-			sensors[1] = true;
-		else
-			sensors[1] = false;
+		if(toutchRightFront.isPressed())
+			sensors += 1;
 		
-		//Color Front
-		if(colorFront.getColorID()==Color.RED){
-			sensors[2] = true;
-			sensors[3] = false;
-		}
-		else if(colorFront.getColorID()==Color.BLACK){
-			sensors[2] = false;
-			sensors[3] = true;
-		}
-		else {
-			sensors[2] = false;
-			sensors[3] = false;
-		}
+		if(toutchLeftFront.isPressed())
+			sensors += 2;
 		
-		//Color Back
-		if(colorBack.getColorID()==Color.RED){
-			sensors[4] = true;
-			sensors[5] = false;
-		}
-		else if(colorBack.getColorID()==Color.BLACK){
-			sensors[4] = false;
-			sensors[5] = true;
-		}
-		else {
-			sensors[4] = false;
-			sensors[5] = false;
+		if(toutchRightBack.isPressed())
+			sensors += 4;
+		
+		if(toutchLeftBack.isPressed())
+			sensors += 8;
+		
+		if(colorFront.getColorID()==Color.RED)
+			sensors += 16;
+		
+		if(colorFront.getColorID()==Color.BLACK)
+			sensors += 32;
+		
+		if(colorBack.getColorID()==Color.RED)
+			sensors += 64;
+		
+		if(colorBack.getColorID()==Color.BLACK)
+			sensors += 128;
+	}
+	
+	/**
+	*PULL THIS OUT TO USE WHEN DECODING
+	*/
+	public static void decode(){
+		
+		//Does the decoding, either add another global variable that this modifies, or find a way to use this in your own way.
+		while(sensors != 0){
+			//COLOR BACK
+			if((sensors - 128) >= 0){
+				sensors -= 128;
+				System.out.println("COLOR BACK        : BLACK");
+			}
+			else if((sensors - 64) >= 0){
+				sensors -= 64;
+				System.out.println("COLOR BACK        : RED");
+			}
+			else
+				System.out.println("COLOR BACK        : white");
+			
+			//COLOR FRONT
+			if((sensors - 32) >= 0){
+				sensors -= 32;
+				System.out.println("COLOR FRONT       : BLACK");
+			}
+			else if((sensors - 16) >= 0){
+				sensors -= 16;
+				System.out.println("COLOR FRONT       : RED");
+			}
+			else
+				System.out.println("COLOR BACK        : white");
+			
+			//TOUCH BACK LEFT
+			if((sensors - 8) >= 0){
+				sensors -= 8;
+				System.out.println("TOUCH BACK LEFT   : TRUE");
+			}
+			else
+				System.out.println("TOUCH BACK LEFT   : false");
+			
+			//TOUCH BACK RIGHT
+			if((sensors - 4) >= 0){
+				sensors -= 4;
+				System.out.println("TOUCH BACK RIGHT  : TRUE");
+			}
+			else
+				System.out.println("TOUCH BACK RIGHT  : false");
+			
+			//TOUCH FRONT LEFT
+			if((sensors - 2) >= 0){
+				sensors -= 2;
+				System.out.println("TOUCH FRONT LEFT  : TRUE");
+			}
+			else
+				System.out.println("TOUCH FRONT LEFT  : false");
+			
+			//TOUCH FRONT RIGHT
+			if((sensors - 1) >= 0){
+				sensors -= 1;
+				System.out.println("TOUCH FRONT RIGHT : TRUE");
+			}
+			else
+				System.out.println("TOUCH FRONT RIGHT : false");
 		}
 	}
 }
