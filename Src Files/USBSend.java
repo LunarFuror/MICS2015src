91344dd3a622438bf509322921dc3dedd3ecbf4f
Grayson Lorenz <lunar_furor@hotmail.com>
2015-03-25 19:03:14 -0500

    Total rewrite
    Works right now

--- Src Files/USBSend.java
+++ Src Files/USBSend.java
@@ -1,157 +1,83 @@
-//PC code
-/**
- * @author Grayson Lorenz
- * built on a heavily edited version of
- * USBSend.java
- * written by Lawrie Griffiths
- */
-package fasdf;
+package org.lejos.pcsample.usbsend;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
-
 import lejos.pc.comm.NXTCommLogListener;
 import lejos.pc.comm.NXTConnector;
- 
-public class USBSend {	
-	
-	public static int sensorCode = 0;
-	public static NXTConnector conn = new NXTConnector();
 
+/**
+ * This is a PC sample. It connects to the NXT, and then sends an integer and
+ * waits for a reply, 100 times.
+ *
+ * Compile this program with javac (not nxjc), and run it with java.
+ *
+ * You need pccomm.jar on the CLASSPATH and the jfantom.dll DLL or liblibnxt.so
+ * shared library on the Java library path.
+ *
+ * Run the program by:
+ *
+ * java USBSend
+ *
+ * Your NXT should be running a sample such as USBReceive.
+ *
+ * @author Lawrie Griffiths
+ *
+ */
+public class USBSendOriginal {
+	public static NXTConnector conn = new NXTConnector();
 	public static DataInputStream inDat;
 	public static DataOutputStream outDat;
-		
-	public static void main(String[] args) 
+	public static void main(String[] args) {
 	
-		//uuuuuh... just leave this working and be sure the NXTBrick program is started BEFORE this program runs.
-		conn.addLogListener(new NXTCommLogListener(){
-
+		conn.addLogListener(new NXTCommLogListener() {
 			public void logEvent(String message) {
-				System.out.println("USBSend Log.listener: "+message);
-				
+				System.out.println("USBSend Log.listener: " + message);
 			}
 
 			public void logEvent(Throwable throwable) {
 				System.out.println("USBSend Log.listener - stack trace: ");
-				 throwable.printStackTrace();
-				
+				throwable.printStackTrace();
 			}
-			
-		} 
-		);
-		
-		
-		
-		if (!conn.connectTo("usb://")){
+		});
+		if (!conn.connectTo("usb://")) {
 			System.err.println("No NXT found using USB");
 			System.exit(1);
 		}
-		
-		//This is where the request magic happens.
 		inDat = new DataInputStream(conn.getInputStream());
 		outDat = new DataOutputStream(conn.getOutputStream());
 		
-		//might not need this part... gotta test it still.
-		while(true)
-		{
-			//a basic wait example, this wouldn't be in the final code, you'd just call the two methods.
-			Scanner scan = new Scanner(System.in);
-			System.out.print("Type and push the enter:");
-			String thing = scan.next();
-						
-			//This sends a request for the sensor data, specifically the writeInt 1, be sure flush is in there
-			requestSensorData();
-//			decode();
+		//Call and get dat shit.
+			getSensor();
+
+		try {
+			inDat.close();
+			outDat.close();
+			System.out.println("Closed data streams");
+		} catch (IOException ioe) {
+			System.err.println("IO Exception Closing connection");
+		}
+		try {
+			conn.close();
+			System.out.println("Closed connection");
+		} catch (IOException ioe) {
+			System.err.println("IO Exception Closing connection");
 		}
 	}
 	
-	/**
-	* This will request the sensor data and update the sensor code variable.
-	*/
-	public static void requestSensorData(){
-		//This sends a request for the sensor data, specifically the writeInt 1, be sure flush is in there
+	public static void getSensor(){
+		int x = 0;
 		try {
-			System.out.print("Writing ");
 			outDat.writeInt(1);
 			outDat.flush();
-			System.out.print("Written ");
 		} catch (IOException ioe) {
 			System.err.println("IO Exception writing bytes");
 		}
-	        
-		//This is where it tries to read the data sent back.
-		//except this for some reason isn't working ever.
-		try {		
-			System.out.println("Available is " + inDat.available());
-			while(inDat.available() != 0){}
-			System.out.println("Available is " + inDat.available());
-			if(inDat.available() != 0){
-				System.out.print("Reading ");
-				sensorCode = inDat.readInt();
-			}
-	    } catch (IOException ioe) {
-	        System.err.println("IO Exception reading reply");
-	    }            
-		
-		//Print sensorCode
-	    System.out.println("Received " + sensorCode);
-	}
-	
-	/**
-	* This will do the math and convert the sensor code into handy dandy sensor... stuff.
-	*/	
-	public static void decode() {
-		// Does the decoding, either add another global variable that this
-		// modifies, or find a way to use this in your own way.
-		while (sensorCode != 0) {
-			// COLOR BACK
-			if ((sensorCode - 128) >= 0) {
-				sensorCode -= 128;
-				System.out.println("COLOR BACK : BLACK");
-			} else if ((sensorCode - 64) >= 0) {
-				sensorCode -= 64;
-				System.out.println("COLOR BACK : RED");
-			} else
-				System.out.println("COLOR BACK : white");
-			
-			// COLOR FRONT
-			if ((sensorCode - 32) >= 0) {
-				sensorCode -= 32;
-				System.out.println("COLOR FRONT : BLACK");
-			} else if ((sensorCode - 16) >= 0) {
-				sensorCode -= 16;
-				System.out.println("COLOR FRONT : RED");
-			} else
-				System.out.println("COLOR BACK : white");
-			
-			// TOUCH BACK LEFT
-			if ((sensorCode - 8) >= 0) {
-				sensorCode -= 8;
-				System.out.println("TOUCH BACK LEFT : TRUE");
-			} else
-				System.out.println("TOUCH BACK LEFT : false");
-			
-			// TOUCH BACK RIGHT
-			if ((sensorCode - 4) >= 0) {
-				sensorCode -= 4;
-				System.out.println("TOUCH BACK RIGHT : TRUE");
-			} else
-				System.out.println("TOUCH BACK RIGHT : false");
-			
-			// TOUCH FRONT LEFT
-			if ((sensorCode - 2) >= 0) {
-				sensorCode -= 2;
-				System.out.println("TOUCH FRONT LEFT : TRUE");
-			} else
-				System.out.println("TOUCH FRONT LEFT : false");
-			
-			// TOUCH FRONT RIGHT
-			if ((sensorCode - 1) >= 0) {
-				sensorCode -= 1;
-				System.out.println("TOUCH FRONT RIGHT : TRUE");
-			} else
-				System.out.println("TOUCH FRONT RIGHT : false");
+		try {
+			x = inDat.readInt();
+		} catch (IOException ioe) {
+			System.err.println("IO Exception reading reply");
 		}
+		System.out.println("Sent " + 1 + " Received " + x);
 	}
 }
